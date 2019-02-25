7d419627f8b23f121ceeef87658202d6ea3e8936
SyntheticAutomation <syntheticauto900@gmail.com>
2019-02-25 15:19:42 -0700

    Adds test for new user post request

---
+++ spec/requests/api/v1/new_user_request_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+describe 'account creation endpoint' do
+  it 'sends api_key' do
+    email = Faker::Internet.email
+    post "/api/v1/users?email=#{email}&password=password&password_confirmation=password"
+    expect(response).to be_successful
+    parsed_response = JSON.parse(response.body)
+    expect(parsed_response["data"]["attributes"]["api_key"].class).to eq(String)
+    User.last.destroy
+  end
+end
