c751b53f1c7dbf711d45ad7c7df77de06611c891
SyntheticAutomation <syntheticauto900@gmail.com>
2019-02-25 16:59:22 -0700

    Adds sessions controller with create action

---
+++ app/controllers/api/v1/sessions_controller.rb
@@ -0,0 +1,13 @@
+class Api::V1::SessionsController < ApplicationController
+
+  def create
+    user = User.find_by(email: params[:email])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      render json: UserSerializer.new(user)
+    else
+      render json: "There was an issue with the credentials you provided. Please try again."
+    end
+  end
+
+end
