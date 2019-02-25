157f552845c6a6b2e99b8e7a54610f46420266b2
SyntheticAutomation <syntheticauto900@gmail.com>
2019-02-25 13:51:22 -0700

    Integrates User class into project

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  describe 'validations' do
+    it { should validate_presence_of(:email) }
+    it { should validate_presence_of(:password_digest) }
+    it { should validate_uniqueness_of(:email) }
+  end
+
+  describe 'relationships' do
+  end
+
+  describe 'class methods' do
+  end
+
+  describe 'instance methods' do
+  end
+end
