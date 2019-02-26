43438b76bbc62a5ba7930ce8ec107d9e6d4fb9c9
SyntheticAutomation <syntheticauto900@gmail.com>
2019-02-26 14:20:17 -0700

    Adds an unused file to coverage ignore list

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -16,6 +16,7 @@ SimpleCov.start 'rails' do
   add_filter "app/jobs/application_job.rb"
   add_filter "app/mailers/application_mailer.rb"
   add_filter "app/helpers/application_helper.rb"
+  add_filter "app/models/application_record.rb"
 end
 
 begin
