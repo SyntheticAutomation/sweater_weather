279d0b5b9c4f48af8c1508a60d9b05df9d9098aa
SyntheticAutomation <syntheticauto900@gmail.com>
2019-02-25 17:10:33 -0700

    Fixes some nomenclature for better legibility

--- spec/requests/api/v1/login_request_spec.rb
+++ spec/requests/api/v1/login_request_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
-describe 'account creation endpoint' do
-  it 'sends api_key' do
+describe 'login endpoint' do
+  it 'can create a session for an existing user' do
     key = SecureRandom.urlsafe_base64
     email = Faker::Internet.email
     user = User.create(email: email, password: "password", api_key: key)
