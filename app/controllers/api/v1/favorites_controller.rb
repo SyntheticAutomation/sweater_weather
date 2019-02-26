e5e21f3478257941de9fe52a59d47711892784d8
SyntheticAutomation <syntheticauto900@gmail.com>
2019-02-25 18:10:18 -0700

    Adds sad path

--- app/controllers/api/v1/favorites_controller.rb
+++ app/controllers/api/v1/favorites_controller.rb
@@ -1,12 +1,12 @@
 class Api::V1::FavoritesController < ApplicationController
 
   def create
-    if params[:api_key]
-      user = User.find_by(api_key: params[:api_key])
+    user = User.find_by(api_key: params[:api_key])
+    if params[:api_key] && user
       favorite_location = user.favorites.create(location: params[:location])
       render json: FavoritesSerializer.new(favorite_location)
     else
-      render json: "Error: Please authorize your request with your API Key."
+      render json: {}, status: 401
     end
   end
 
