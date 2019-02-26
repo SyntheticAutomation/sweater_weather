572ccefa9bdda8b9feff8f3466eeb4e94b30e6bc
SyntheticAutomation <syntheticauto900@gmail.com>
2019-02-25 18:10:10 -0700

    Adds sad path

--- spec/requests/api/v1/favorites_request_spec.rb
+++ spec/requests/api/v1/favorites_request_spec.rb
@@ -5,9 +5,16 @@ describe 'favorites endpoint' do
     key = SecureRandom.urlsafe_base64
     email = Faker::Internet.email
     user = User.create(email: email, password: "password", api_key: key)
+
     post "/api/v1/favorites?location=miami,fl&api_key=#{key}"
+
     expect(response).to be_successful
     parsed_favorite = JSON.parse(response.body, symbolize_names: true)[:data][:attributes]
     expect(parsed_favorite[:location]).to eq("miami,fl")
+  end
+  it 'returns 401 when invalid user' do
+    post "/api/v1/favorites?location=miami,fl&api_key=000"
+
+    expect(status).to be(401)
   end
 end
