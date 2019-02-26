503d777989a654585f65584b4623bc2d7fb47d9c
SyntheticAutomation <syntheticauto900@gmail.com>
2019-02-25 17:58:45 -0700

    Adds request test for favorites endpoint

--- spec/requests/api/v1/favorites_request_spec.rb
+++ spec/requests/api/v1/favorites_request_spec.rb
@@ -5,7 +5,9 @@ describe 'favorites endpoint' do
     key = SecureRandom.urlsafe_base64
     email = Faker::Internet.email
     user = User.create(email: email, password: "password", api_key: key)
-    post "/api/v1/favorites?location=denver,co&api_key=#{key}"
+    post "/api/v1/favorites?location=miami,fl&api_key=#{key}"
     expect(response).to be_successful
+    parsed_favorite = JSON.parse(response.body, symbolize_names: true)[:data][:attributes]
+    expect(parsed_favorite[:location]).to eq("miami,fl")
   end
 end
