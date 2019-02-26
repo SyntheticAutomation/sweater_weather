91adb7b04a2f1e5562e96626541119dc78ea1297
SyntheticAutomation <syntheticauto900@gmail.com>
2019-02-26 11:27:05 -0700

    Adds test suite for favorites request

--- spec/requests/api/v1/favorites_request_spec.rb
+++ spec/requests/api/v1/favorites_request_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 describe 'favorites endpoint' do
-  it 'can create a favorite location for an existing user' do
+  it 'can create a favorite location for an existing user', :vcr do
     key = SecureRandom.urlsafe_base64
     email = Faker::Internet.email
     user = User.create(email: email, password: "password", api_key: key)
@@ -12,14 +12,45 @@ describe 'favorites endpoint' do
     parsed_favorite = JSON.parse(response.body, symbolize_names: true)[:data][:attributes]
     expect(parsed_favorite[:location]).to eq("miami,fl")
   end
+
   it 'returns 401 when invalid user' do
     post "/api/v1/favorites?location=miami,fl&api_key=000"
 
     expect(status).to be(401)
   end
+
   it 'returns 401 when no api_key given' do
     post "/api/v1/favorites?location=miami,fl"
 
     expect(status).to be(401)
   end
+
+  it 'sends all favorites for existing user', :vcr do
+    key = SecureRandom.urlsafe_base64
+    email = Faker::Internet.email
+    user = User.create(email: email, password: "password", api_key: key)
+    post "/api/v1/favorites?location=miami,fl&api_key=#{key}"
+    post "/api/v1/favorites?location=denver,co&api_key=#{key}"
+    post "/api/v1/favorites?location=montgomery,al&api_key=#{key}"
+
+    get "/api/v1/favorites?api_key=#{user.api_key}"
+    expect(response).to be_successful
+    parsed_list = JSON.parse(response.body, symbolize_names: true)
+    expect(parsed_list[:data][0]][:attributes][:location]).to eq('miami,fl')
+    expect(parsed_list[:data][1][:attributes][:location]).to eq('denver,co')
+    expect(parsed_list[:data][1][:attributes][:current_weather][:summary].class).to eq(String)
+  end
+
+  it 'returns 401 when invalid user' do
+    post "/api/v1/favorites?api_key=000"
+
+    expect(status).to be(401)
+  end
+
+  it 'returns 401 when no api_key given' do
+    post "/api/v1/favorites"
+
+    expect(status).to be(401)
+  end
+
 end
