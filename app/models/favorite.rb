bc535695bfbd2376ec8982cf26cb82b1e05abf19
SyntheticAutomation <syntheticauto900@gmail.com>
2019-02-26 11:28:42 -0700

    Adds current_weather to favorites api response

--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -1,4 +1,12 @@
 class Favorite < ApplicationRecord
   validates_presence_of :location
   belongs_to :user
+
+  def current_weather
+    raw_city = location.split(",")[0]
+    raw_city.include?(" ") ? city = raw_city.gsub(' ', '%20') : city = raw_city
+    state = location[-2..-1]
+    forecast = Forecast.new(city, state)
+    current = forecast.right_now
+  end
 end
